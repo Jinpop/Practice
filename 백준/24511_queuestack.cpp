f643ded2715be4a41963acee6ac1f3f058d8c6e2
Sungjin Park <qktjwl123@gmail.com>
2024-03-07 14:49:14 +0900

    fourth commit

---
+++ 백준/24511_queuestack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+#include <deque>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n; 
+
+    for (int i = 0; i < n; i++){
+        cin
+    }
+
+    return 0;
+}
